7274277cfdf57fef19501e773ee4e024dc840905
SNUBeaver <kwonij2@snu.ac.kr>
2018-04-25 16:51:17 +0900

    added function to apiview

--- src/profile_rest/profile_api/views.py
+++ src/profile_rest/profile_api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
+
 
 # Create your views here.
 '''
@@ -15,6 +19,9 @@ ViewSet :
 
 class HelloApiView(APIView):
     """Test Api View"""
+
+    serializer_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         apiview = [
             'Uses HTTP method as function',
@@ -24,3 +31,25 @@ class HelloApiView(APIView):
         ]
         # REsponse always shoulf be in dictionary
         return Response({'message':'hello!', 'apiview': apiview})
+
+    def post(self, request):
+        # request를 serializer에 보냄
+        serializer= serializers.HelloSerializer(data=request.data)
+
+        # check if data is valid to serializer
+        if serializer.is_valid():
+            # once it is sent to serializer we can get data by dic
+            name= serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status= status.HTTP_400_BAD_REQUEST)
+    # pk is primary key, identify id of object key
+    def put(self, request, pk=None):
+        return Response({'method':'put'})
+
+    def patch(self, request, pk=None):
+        return Response({'method':'patch'})
+
+    def delete(self, request, pk=None):
+        return Response({'method':'delete'})
