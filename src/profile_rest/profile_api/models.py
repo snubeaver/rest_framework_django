07f25220951a454ef6aa5f69286b521c1b54835e
SNUBeaver <kwonij2@snu.ac.kr>
2018-04-26 13:58:35 +0900

    added model for store Profile Feed

--- src/profile_rest/profile_api/models.py
+++ src/profile_rest/profile_api/models.py
@@ -63,3 +63,13 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         convert object to string
         """
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """PRofile status update"""
+
+    user_profile = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.status_text
